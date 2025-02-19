86db6051355ed0d102424bd77f2d9d911b5f8ad9
AzerQ <46249148+AzerQ@users.noreply.github.com>
2025-02-19 07:32:57 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 # Build docker image
 docker build -t packagedownloader .
 
+# Remove old container
+docker stop packagedownloader_app
+docker rm packagedownloader_app
+
 # Run container
-docker run --env AI__API_KEY=YOUR_API_KEY -d -p 80:80 packagedownloader
+docker run --name packagedownloader_app  --env AI__API_KEY -d -p 80:80

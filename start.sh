3f600d0bd25d25ee96695dc37496eb2e8f3e8565
AzerQ <46249148+AzerQ@users.noreply.github.com>
2025-02-19 07:52:55 +0300

    Update start.sh (Fix container start command)

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ docker stop packagedownloader_app
 docker rm packagedownloader_app
 
 # Run container
-docker run --name packagedownloader_app  --env AI__API_KEY -d -p 80:80
+docker run --name packagedownloader_app  --env AI__API_KEY -d packagedownloader:latest  -p 80:80

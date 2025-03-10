2b6758090fb393ffb00ef98af53e93ccf86786a1
AzerQ <xxxvorobej71@gmail.com>
2025-03-10 20:09:25 +0300

    Refactor App component to use PackagesDownloadPage for improved UI layout. Organize components within Grid for better visual separation. Introduce PreviewReadme component in PackagesDownloadPage for enhanced user experience.

---
+++ Frontend/package-downloader-react/src/pages/PackagesDownloadPage.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Box, Container, Grid, Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import PackageCart from '../components/Cart/PackageCart';
+import NotificationBanner from '../components/Notification/Notification';
+import PreviewReadme from '../components/ReadmePreview';
+import RecommendationsModal from '../components/Recommendations/RecommendationsModal';
+import SearchForm from '../components/SearchForm/SearchForm';
+import LanguageSwitcher from '../localization/LanguageSwitcher';
+
+
+const PackagesDownloadPage: React.FC = () => {
+
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <LanguageSwitcher />
+
+      <Grid container spacing={2}>
+        <Grid item xs={6} sx={{ overflowY: "scroll", maxHeight: "80em" }}>
+          <Container maxWidth="xl">
+            <Typography variant="h4" align="center" gutterBottom>
+              {t("AppTitle")}
+            </Typography>
+
+            <SearchForm />
+            <PackageCart />
+            <RecommendationsModal />
+            <NotificationBanner />
+
+          </Container>
+        </Grid>
+        <Grid item xs={6} sx={{ overflowY: "scroll", maxHeight: "80em" }}>
+          <PreviewReadme />
+        </Grid>
+      </Grid>
+
+    </>
+  );
+};
+
+export default PackagesDownloadPage;

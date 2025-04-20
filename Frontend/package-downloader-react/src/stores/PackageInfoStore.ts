dbab2da800a6f226368212c299245a7f2fe5abb6
Anton Vorobev <azerqdev@gmail.com>
2025-04-20 21:34:53 +0300

    Refactor PackageInfoStore to use mobx-utils fromPromise for readme loading

--- Frontend/package-downloader-react/src/stores/PackageInfoStore.ts
+++ Frontend/package-downloader-react/src/stores/PackageInfoStore.ts
@@ -1,44 +1,26 @@
-import { makeAutoObservable, runInAction } from "mobx";
-import { githubApiClient } from "../services/githubApi";
-import { notificationStore } from "./NotificationStore";
+import {makeAutoObservable} from "mobx";
+import {githubApiClient} from "../services/githubApi";
+import {fromPromise, IPromiseBasedObservable} from "mobx-utils";
 
 export class PackageInfoStore {
 
     repositoryUrl: string = '';
-    readmeContent: string = '';
-    isReadmeLoading: boolean = false;
+    readmeContent?: IPromiseBasedObservable<string>;
 
     constructor() {
         makeAutoObservable(this);
     }
 
-
     fetchReadmeContent = async (repositoryUrl: string) => {
-
         if (!repositoryUrl)
             return;
 
-        try {
-            this.isReadmeLoading = true;
-            const readmeContent = await githubApiClient.getReadmeContent(repositoryUrl);
-            console.log(readmeContent);
-            
-            runInAction(() => {
-                this.repositoryUrl = repositoryUrl
-                this.readmeContent = readmeContent;
-                this.isReadmeLoading = false;
-            });
-        }
-
-        catch (error) {
-            this.isReadmeLoading = false;
-            notificationStore.addError(`Error while loading package readme: ${error}`);
-        }
-    };
+        this.readmeContent = fromPromise(githubApiClient.getReadmeContent(repositoryUrl));
+    }
 
     clearPackageInfo = () => {
         this.repositoryUrl = '';
-        this.readmeContent = '';
+        this.readmeContent = undefined;
     }
 
 }

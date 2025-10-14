a40f90fcdc360707c79f9f04aa9010d072a6f4f2
AzerQ <46249148+AzerQ@users.noreply.github.com>
2025-10-14 09:56:31 +0000

    Исправлено получение содержимого README: добавлена обработка URL репозитория и улучшена декодировка содержимого с использованием base64Unicode.

--- Frontend/package-downloader-react/src/services/githubApi.ts
+++ Frontend/package-downloader-react/src/services/githubApi.ts
@@ -8,8 +8,9 @@ export class GitHubApi implements IGithubAPI {
 
     getReadmeContent = async (repoUrl: string): Promise<string> => {
 
+        const clearRepoUrl = repoUrl?.replace(".git", "");
 
-        const match = repoUrl.match(/https:\/\/github\.com\/([^/]+)\/([^/]+)/);
+        const match = clearRepoUrl.match(/https:\/\/github\.com\/([^/]+)\/([^/]+)/);
 
         
         if (!match)
@@ -21,11 +22,30 @@ export class GitHubApi implements IGithubAPI {
 
         const url = `https://api.github.com/repos/${owner}/${repo}/readme`;
         const response = await axios.get<{ content: string }>(url);
-        const readmeContent = atob(response.data.content);
+        const readmeContent = base64Unicode.Decode(response.data.content);
 
         return readmeContent;
     }
 
+}
+
+const base64Unicode = {
+    Encode(str: string) {
+        // first we use encodeURIComponent to get percent-encoded UTF-8,
+        // then we convert the percent encodings into raw bytes which
+        // can be fed into btoa.
+        return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+            function toSolidBytes(_match: string, p1: string) {
+                return String.fromCharCode(('0x' + p1) as unknown as number);
+            }));
+    },
+
+    Decode(str: string) {
+        // Going backwards: from bytestream, to percent-encoding, to original string.
+        return decodeURIComponent(atob(str).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    }
 }
 
 export const githubApiClient: IGithubAPI = new GitHubApi();
